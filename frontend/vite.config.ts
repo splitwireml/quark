45c15674b6a76a043629862c2d53241d02d0b44e
masterbatcoderman10 <suhaib.athar@gmail.com>
2026-07-13 11:24:59 +0400

    chore: scaffold FastAPI and Svelte projects

---
+++ frontend/vite.config.ts
@@ -0,0 +1,7 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [svelte()],
+  server: { proxy: { '/api': 'http://127.0.0.1:8000' } }
+});
